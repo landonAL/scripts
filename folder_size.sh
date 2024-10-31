c32e4a16a2c33b0ca7038bdfea87a88364e6234a
Landon <92648744+landonAL@users.noreply.github.com>
2024-10-31 15:03:36 -0500

    Create folder_size.sh

---
+++ folder_size.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# They must provide only one file
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
+# They did not provide a valid directory
+if [ ! -d $1 ]; then
+    echo "$1 is not a directory"
+fi
+
+# Collect all directories
+directories=$(
+  find "$1" -maxdepth 1 -type d | while read -r dir; do
+    if [[ "$dir" != "$1" ]]; then
+      sudo du -hs "$dir"
+    fi
+  done
+)
+
+# Sort and print directories from highest to lowest size
+echo "$directories" | sort -hr
